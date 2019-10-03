d71c68e9b20ba0d66e02540de85e966cb76dd429
Kohlton <booth.kohl@gmail.com>
2019-10-02 22:09:50 -0600

    added functionality to canadaByRepresentative.py Now gets all representatives, and sorts them by how much they rebel against party lines

--- getVoteData/canadaByRepresentative.py
+++ getVoteData/canadaByRepresentative.py
@@ -32,8 +32,12 @@ class Vote(object):
             voteID = (parliament #, session #, vote #)
             voteResult = {party: (num yes, num no)}
         """
-        self.voteTuple = voteID
+        self.voteID = voteID
         self.voteResult = result
+
+    def __str__(self):
+        tempList = [str(x) for x in self.voteTuple]
+        return "_".join(tempList) + " " + str(self.voteResult)
 
 class Representative(object):
     def __init__(self, name, constituency, party, province):
@@ -56,23 +60,91 @@ class Representative(object):
     def __str__(self):
         return "name: %s, consituency: %s, party: %s, province: %s" % (self.name, self.constituency, self.party, self.province)
 
+    def voteString(self):
+        outputString = ""
+        for v in self.votes:
+            outputString += ("vote summary: %s, representative vote: %d\n" % v)
+        return outputString
+
     def __eq__(self, otherRep):
         return self.name == otherRep.name
 
     def addVote(self, vote, yeaNay):
         self.votes.append((vote, yeaNay))
 
-allReps = []
-repNames = [] # check against rep names to see if they're already in the database
+
+def analyzeVotes(voteDict):
+    allParties = {}
+    for participant in voteDict:
+        party = participant["PartyName"]
+        votedYes = participant["Yea"]
+        votedNo = participant["Nay"]
+        if not party in allParties:
+            allParties[party] = [0,0]
+
+        if votedYes == "1":
+            yesVotes = allParties[party][0]
+            allParties[party][0] = yesVotes + 1
+        elif votedNo == "1":
+            noVotes = allParties[party][1]
+            allParties[party][1] = noVotes + 1
+    return allParties
+
+allReps = {}
+floorCrossers = [] # if they ever cross the floor don't analyze them
 
 for voteFile in cleanFileNames:
     with open(voteFile) as f:
+        print(voteFile)
         resultDict = xmltodict.parse(f.read())
         allVotesList = resultDict["List"]["VoteParticipant"]
+        voteMetaData = voteFile.split("/")[-1][:-4].split("_")
+        voteMetaData = (int(voteMetaData[0]), int(voteMetaData[1]), int(voteMetaData[2]))
+        
+        voteSummary = analyzeVotes(allVotesList)
+        voteOb = Vote(voteMetaData, voteSummary)
+
         for voterRecord in allVotesList:
-            if not voterRecord["Name"] in repNames:
-                newRep = Representative(voterRecord["Name"], voterRecord["ConstituencyName"], voterRecord["PartyName"], voterRecord["Province"])
-                repNames.append(voterRecord["Name"])
+            currentRepName = voterRecord["Name"]
+            if currentRepName in floorCrossers:
+                continue
+            if not currentRepName in allReps:
+                newRep = Representative(voterRecord["Name"], voterRecord["ConstituencyName"],
+                                        voterRecord["PartyName"], voterRecord["Province"])
+                allReps[currentRepName] = newRep
+            rep = allReps[currentRepName]
+
+            # if they ever switch parties delete them from the database
+            if rep.party != voterRecord["PartyName"]:
+                del allReps[currentRepName]
+                floorCrossers.append(currentRepName)
+            
+            rep.addVote(voteOb, int(voterRecord["Yea"]))
+
+
+repsByNumRebellions = {}
+for rep in allReps:
+    party = allReps[rep].party
+    totalVotes = len(allReps[rep].votes)
+    numRebellions = 0
+    for vote in allReps[rep].votes:
+        partyVotedYea = 0
+        if vote[0].voteResult[party][0] < vote[0].voteResult[party][1]:
+            partyVotedYea = 1
+        if partyVotedYea == vote[1]:
+            numRebellions += 1
+
+    if not numRebellions in repsByNumRebellions:
+        repsByNumRebellions[numRebellions] = []
+    
+    repsByNumRebellions[numRebellions].append(rep)
+
+for repList in sorted(list(repsByNumRebellions.keys())):
+    print(repList)
+    print("\t" + str(repsByNumRebellions[repList]))
+    print()
+        
+
 
 
 
